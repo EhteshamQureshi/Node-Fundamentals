9cfa771d1a44b95583e8af47a1e41ef32850200c
Ehtesham Qureshi <ehteshamqureshi.101@gmail.com>
2021-07-27 10:58:15 +0500

    Node JS Modules

---
+++ app.js
@@ -0,0 +1,12 @@
+//node has global varialble rather than window Object.
+console.log('Hello via global console method');
+
+// setTimeout()
+// clearTimeout();
+
+// setInterval();
+// clearInterval();
+
+// var message="I am not accessible to global above methods";
+console.log(module)
+// In node every file is a module and dunction, varialble defined in that module are scoped to that file..
