b2d1af7e8425279105ff5adf42156da2369f196a
Ehtesham Qureshi <ehteshamqureshi.101@gmail.com>
2021-07-27 11:28:22 +0500

    Exporting module and single fuction module.exports and  require is used

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //node has global varialble rather than window Object.
-console.log('Hello via global console method');
+// console.log('Hello via global console method');
 
 // setTimeout()
 // clearTimeout();
@@ -8,5 +8,18 @@ console.log('Hello via global console method');
 // clearInterval();
 
 // var message="I am not accessible to global above methods";
-console.log(module)
+// console.log(module)
 // In node every file is a module and dunction, varialble defined in that module are scoped to that file..
+
+// Loading Module
+// var logger = require("/logger"); //require('/logger.js'); same as logger,logger.js
+
+// const logger = require("./logger"); 
+// logger.log('message');
+ 
+// set to constant so that we can not accidently change the value of the exported values
+// for smae folder just use / if going to parent folder use ..  or /subfolder/filename
+
+// Now uisng the exported single function so it is no longer object
+const logger = require("./logger"); 
+logger('I am single funciton exported');
--- logger.js
+++ logger.js
@@ -1,7 +1,6 @@
-var url="http://logger.io/log/"
+var url = "http://logger.io/log/";
 
-
-function log(message){
+function log(message) {
     // Send http request
     console.log("I am exported");
     console.log(message);
@@ -9,8 +8,11 @@ function log(message){
 // to make this above function visible outside this module for app.js module we will need to make it publid
 
 // using module export property
-module.exports.log =log;
+// module.exports.log = log;
+//Here above the o bject is exported but if we don't need to export object we can jsut export the specific function like below
+
 // module.exports.endPoint = url; // we can also give new name while exporting the variables like endPoint for url
-// we don't need to provide implementation detial like we are doing with varilable above in the url so keep url private 
+// we don't need to provide implementation detial like we are doing with varilable above in the url so keep url private
 
-
+// Now exporitng and using a single function
+module.exports = log;
